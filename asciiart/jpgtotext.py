c85153ad0e32931e4c2911c3297f388006e94af9
and-pad <andrestor@gmail.com>
2026-06-12 17:37:54 -0600

    jp2a change caracter

--- asciiart/jpgtotext.py
+++ asciiart/jpgtotext.py
@@ -3,7 +3,7 @@ import subprocess
 
 class asciijpg:
     def convert(self, image_path, width=None, height=None, colors=False):
-        command = ["/usr/bin/jp2a", "--html", "--chars= .'`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$%█"]
+        command = ["/usr/bin/jp2a", "--html", "--chars= .'`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$%"]
 
         if colors:
             command.append("--colors")
